70b660a653319f3b0b5b60b7d985cde89d3ec87f
yashasrnair <yashasrnair@gmail.com>
2024-08-28 21:08:24 +0530

    mrquee fix error

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CardItem(props) {
-return (
+  return (
     <>
-    <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-        <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-            className='cards__item__img'
-            alt='Travel '
-            src={props.src}
-            />
-        </figure>
-        <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
-        </div>
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
+            <img className="cards__item__img" alt="Travel " src={props.src} />
+            <span className="cards__item__label">{props.label}</span>
+          </figure>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
+          </div>
         </Link>
-    </li>
+      </li>
     </>
-);
+  );
 }
 
 export default CardItem;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,54 +1,87 @@
 import React from "react";
 import "./Cards.css";
 import CardItem from "./CardItem";
-import Carousel from "react-bootstrap/Carousel";
+import Marquee from "react-fast-marquee";
+// import Carousel from "react-bootstrap/Carousel";  https://jj5vh794-3000.inc1.devtunnels.ms/
 
 function Cards() {
   return (
     <div className="cards">
       <h1>Check out these EPIC Destinations!</h1>
-      <Carousel>
-        <Carousel.Item>
-          <CardItem
-            src="/images/mem.jpg"
-            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-            label="Adventure"
-            path="/services"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <CardItem
-            src="/images/mem1.jpg"
-            text="Travel through the Islands of Bali in a Private Cruise"
-            label="Luxury"
-            path="/services"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <CardItem
-            src="images/mem7.jpg"
-            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-            label="Mystery"
-            path="/services"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <CardItem
-            src="/images/mem5.jpg"
-            text="Experience Football on Top of the Himilayan Mountains"
-            label="Adventure"
-            path="/products"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <CardItem
-            src="/images/mem6cr.jpg"
-            text="Ride through the Sahara Desert on a guided camel tour"
-            label="Adrenaline"
-            path="/sign-up"
-          />
-        </Carousel.Item>
-      </Carousel>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <Marquee autoFill='True'>
+              <CardItem
+                src="/images/mem1.jpg"
+                text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                label="Adventure"
+                path="/services"
+              />
+              <CardItem
+                src="/images/mem4.jpg"
+                text="Travel through the Islands of Bali in a Private Cruise"
+                label="Luxury"
+                path="/services"
+              />
+              <CardItem
+                src="/images/mem3.jpg"
+                text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                label="Mystery"
+                path="/services"
+              />
+              <CardItem
+                src="/images/mem5.jpg"
+                text="Experience Football on Top of the Himilayan Mountains"
+                label="Adventure"
+                path="/products"
+              />
+              <CardItem
+                src="/images/mem6.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/mem7.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/mem.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/mem8.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/img-1.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/img-2.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+              <CardItem
+                src="/images/img-8.jpg"
+                text="Ride through the Sahara Desert on a guided camel tour"
+                label="Adrenaline"
+                path="/sign-up"
+              />
+            </Marquee>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
