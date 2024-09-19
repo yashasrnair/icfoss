cac8a0ae05e4c941cf81e58930887640dca0af7e
yashasrnair <yashasrnair@gmail.com>
2024-09-19 16:13:42 +0530

    commit

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ import Marquee from "react-fast-marquee";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>The Team</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <Marquee direction="left" 
@@ -17,67 +17,67 @@ function Cards() {
                 src="/images/mem1.jpg"
                 text="Explore the hidden waterfall deep inside the Amazon Jungle"
                 label="Adventure"
-                path="/services"
+                path="/"
               />
               <CardItem
                 src="/images/mem4.jpg"
                 text="Travel through the Islands of Bali in a Private Cruise"
                 label="Luxury"
-                path="/services"
+                path="/"
               />
               <CardItem
                 src="/images/mem3.jpg"
                 text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
                 label="Mystery"
-                path="/services"
+                path="/"
               />
               <CardItem
                 src="/images/mem5.jpg"
                 text="Experience Football on Top of the Himilayan Mountains"
                 label="Adventure"
-                path="/products"
+                path="/"
               />
               <CardItem
                 src="/images/mem6.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/mem7.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/mem.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/mem8.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/img-1.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/img-2.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
               <CardItem
                 src="/images/img-8.jpg"
                 text="Ride through the Sahara Desert on a guided camel tour"
                 label="Adrenaline"
-                path="/sign-up"
+                path="/"
               />
           </ul>
           </Marquee>
